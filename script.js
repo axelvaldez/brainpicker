45cc0cd6870b59e18a3a91d1112a16c74709d596
Axel Valdez <axel@paskola.com>
2017-07-31 02:08:53 -0700

    Quick UI Changes

--- script.js
+++ script.js
@@ -2,52 +2,66 @@ $(function(){
 
     // Read questions from text file
     function getQuestions(){
-        var questions = new Array;
-
         $.get('questions.txt', function(data){
-            questions = data.split('\n');
+            var questions = data.split('\n');
 
             $.each(questions, function(i, question){
                 if (question != ''){
-                    $('#questions').append('<p>' + question + '</p>');
+                    $('#questions').append('<p class="questions__question">' + question + '</p>');
                 }
             });
+            setUpQuestions();
         });
     }
 
+    //shuffleChildren (taken from https://css-tricks.com/snippets/jquery/shuffle-children/)
+    $.fn.shuffleChildren = function(){
+      $.each(this.get(), function(index, el){
+          var $el = $(el);
+          var $find = $el.children();
+
+          $find.sort(function() {
+              return 0.5 - Math.random();
+          });
+
+          $el.empty();
+          $find.appendTo($el);
+      });
+    };
+
+    // setup questions
+    function setUpQuestions(){
+      $("#questions").css('width', ($('.questions__question').length * 100) + '%');
+      $("#questions").shuffleChildren();
+    }
+
     // Switch to next question
-    function next(){
-        $('#questions p:first-child').appendTo('#questions');
+    function go(direction){
+      if (direction == 'next' && current < $('.questions__question').length - 1){
+        current = current + 1;
+      }
+      if (direction == 'prev' && current > 0){
+        current = current - 1;
+      }
+      $("#questions").css('left', '-' + current * $(window).width() + 'px');
     }
 
-    // Switch to previous question
-    function prev(){
-        $('#questions p:last-child').prependTo('#questions');
-    }
 
-    $('.next').click(next);
-    $('.prev').click(prev);
+    $('.controls__next').click(function(){go('next');});
+    $('.controls__prev').click(function(){go('prev');});
 
-    $(window).on("swiperight", next);
-    $(window).on("swipeleft", prev);
+    $(window).on("swiperight", function(){go('next');});
+    $(window).on("swipeleft", function(){go('prev');});
 
     // Key Bindings (arrow keys)
     $(document).keydown(function(e) {
         switch(e.which) {
             // left
-            case 37: prev();
-            break;
-
-            // up
-            case 38: prev();
+            case 37: go('prev');
             break;
 
             // right
-            case 39: next();
-            break;
-
-            // down
-            case 40: next();
+            case 39: go('next');
             break;
 
             default: return;
@@ -55,5 +69,6 @@ $(function(){
         e.preventDefault();
     });
 
+    var current = 0;
     getQuestions();
 });
